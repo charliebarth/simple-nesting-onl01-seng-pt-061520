60959131a5bcc787242dbf3771d3e12f57352fe1
Charlie Barth <charliepbarth@gmail.com>
2020-06-28 01:39:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,7 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] + "Assembly"
-puts programmer_hash[:dennis_ritchie][:languages]
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+return programmer_hash
 end
-adding_to_dennis
